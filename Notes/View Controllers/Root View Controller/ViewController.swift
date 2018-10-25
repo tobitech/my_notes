33e448892fcbc9336f0fa62e0a4adb846584c290
Oluwatobi Omotayo <tobitech@ymail.com>
2018-10-25 16:15:57 +0100

    Initial Commit

---
+++ Notes/View Controllers/Root View Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Bart Jacobs on 23/11/2017.
+//  Copyright © 2017 Cocoacasts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
