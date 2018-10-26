2174b9b500cbf20c5123b2f8996aa3854138e396
Oluwatobi Omotayo <tobitech@ymail.com>
2018-10-26 13:13:10 +0100

    use dependency injection to inject coredata manager class into the root view controller

--- Notes/View Controllers/Root View Controller/ViewController.swift
+++ Notes/View Controllers/Root View Controller/ViewController.swift
@@ -9,15 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let coreDataManager = CoreDataManager(modelName: "Notes")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        print(coreDataManager.managedObjectContext)
+        
     }
 
 
