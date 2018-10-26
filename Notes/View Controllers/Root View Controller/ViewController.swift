767b5ee7ed24697b1421891065976d14a1b10323
Oluwatobi Omotayo <tobitech@ymail.com>
2018-10-26 16:24:11 +0100

    add some relationships and configure those relations for the data model,

--- Notes/View Controllers/Root View Controller/ViewController.swift
+++ Notes/View Controllers/Root View Controller/ViewController.swift
@@ -7,15 +7,40 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
     
+    // MARK: - Properties
+    
     private let coreDataManager = CoreDataManager(modelName: "Notes")
 
+    // MARK: - View Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(coreDataManager.managedObjectContext)
+        if let entityDescription = NSEntityDescription.entity(forEntityName: "Note", in: coreDataManager.managedObjectContext) {
+            
+            print(entityDescription.name ?? "No Name")
+            print(entityDescription.properties)
+            
+            let note = NSManagedObject(entity: entityDescription, insertInto: coreDataManager.managedObjectContext)
+            
+            note.setValue("My First Note", forKey: "title")
+            note.setValue(Date(), forKey: "createdAt")
+            note.setValue(Date(), forKey: "updatedAt")
+            
+            print(note)
+            
+            do {
+                try coreDataManager.managedObjectContext.save()
+            } catch {
+                print("Unable to Save Managed Object Context")
+                print("\(error), \(error.localizedDescription)")
+            }
+            
+        }
         
     }
 
